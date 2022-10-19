4c982db4b6a99339bdb868773f6a0315db0e8128
mikedaltonmtl <michael.j.dalton@gmail.com>
2022-10-19 12:46:20 +0000

    add edge case breed not found to breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,20 +8,30 @@ if (!breed) {
   return;
 }
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian'; 
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`; 
 
 request(url, (error, response, body) => {
 
-  // console.log(typeof body);
-  // console.log('body', body);
+  // print any errors
+  if (error) {
+    console.log('error', error);
+  }
+
+  // print the response status code if a response was received
+  // console.log('statusCode:', response && response.statusCode);
 
   // body is a string, we can deserialize it into an object using JSON.parse()
   const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
 
-  // print description only
+  // check that some data has been returned
+  // (an empty array is returned if the breed is not found)
+  if (data.length === 0) {
+    console.log('The breed you chose does not exist in the database, sorry.');
+    return;
+  }
+
+  // we have data, print the description
   const desc = data[0]['description'];
-  console.log('desc', desc);
+  console.log(desc);
 
 });
